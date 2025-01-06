dab31d8e7630a63d696ed86e536ca6ca8109819f
Limselect <seontaek1998@gmail.com>
2025-01-06 22:26:22 +0900

    250106 test

---
+++ src/com/in28minutes/oops/MotorBike.java
@@ -0,0 +1,13 @@
+package com.in28minutes.oops;
+
+public class MotorBike {
+	private int speed;
+
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	void start() {
+		System.out.println("Bike Started");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module introduction_to_object_oriented_programming {
+}
